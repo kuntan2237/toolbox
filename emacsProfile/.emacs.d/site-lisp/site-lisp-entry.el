1b543bf7409dfe7ba04cfc2baa5bef35868b7488
KUNTAN2237 <kuntan2237@gmail.com>
2016-02-24 22:19:19 +0800

    Add emacs ztree package

---
+++ emacsProfile/.emacs.d/site-lisp/site-lisp-entry.el
@@ -0,0 +1,11 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Kun's Emacs Profile
+;;; Site LIST Entry
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; ==========Compare Directory==========
+;; https://github.com/fourier/ztree.git
+(add-to-list 'load-path' "~/.emacs.d/ztree")
+(require 'ztree)
+
+(provide 'site-lisp-entry)
