c8884769c92a73455b8511f4879f780985bdb0d2
Kun Tan <kuntan2237@gmail.com>
2016-03-04 23:51:53 +0800

    Bug Fix: mine.sh won't reload after time interval change

--- autoMine/mine.sh
+++ autoMine/mine.sh
@@ -3,7 +3,6 @@
 pushd $(dirname ${BASH_SOURCE[*]})
 while true; do
     source ./manual &
-    sleep $[60 * 60 * 8] # 8 hrs
     pgrep cpuminer | xargs kill
     pushd ..; git pull origin master; popd
 done
