8ea3676eeb2a3e1f07a5240d2504c8badc2a8100
Kun Tan <kuntan2237@gmail.com>
2016-03-05 14:16:47 +0800

    Feature: update proxy check

--- autoMine/mine.sh
+++ autoMine/mine.sh
@@ -3,17 +3,56 @@
 INTERVAL=10
 DIR=$(dirname ${BASH_SOURCE[*]})
 
-source $DIR/commands &
+getLatency() {
+    local latency=`ping -c $INTERVAL -s 128 speedtest.$1.nicehash.com | grep rtt | awk -F '/' {'print $5'}`
+    if [ x"$1" == x"hk" ]; then
+        echo "$latency+150" | bc
+    elif [ x"$1" == x"jp" ]; then
+	echo "$latency+100" | bc
+    else
+        echo $latency
+    fi
+}
+
+getProxy() {
+    local bestProxy=$1
+    local bestLatency=`getLatency $1`
+
+    for proxy in "hk" "jp" "usa" "eu"; do
+        if [ x$bestProxy == x$proxy ]; then continue; fi
+
+	local latency=`getLatency $proxy`
+        if [ $(echo $bestLatency'>'$latency'+10' | bc) == 1 ]; then
+            bestProxy=$proxy
+            bestLatency=$latency
+	    fi
+    done
+
+    echo $bestProxy
+}
+
+if [ x$1 == x ]; then
+    currProxy="hk"
+else
+    currProxy=$1
+fi
+
+source $DIR/commands $currProxy &
 
 # wait if there's no update
 pushd $DIR
 while git pull origin autoMine 2>&1 | grep "Already up-to-date" > /dev/null; do
-    sleep $INTERVAL
+    # Check fastest proxy
+    bestProxy=`getProxy $currProxy`
+    if [ x$bestProxy != x$currProxy ]; then
+	echo =====PROXY CHANGED!=====
+	break;
+    fi
 done
 popd
 
 # script updated, reload commands
 pgrep cpuminer | xargs kill
 sleep $INTERVAL
-exec $DIR/$(basename $0)
+exec $DIR/$(basename $0) $bestProxy
 
