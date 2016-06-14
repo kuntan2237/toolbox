f1e0b93eaabda9d02cea08d3038839b799bed6d1
kuntan <kuntan@cspg-diag-01.(none)>
2016-06-14 02:00:39 -0700

    Update Emacs 23.1.1 profile, used in CentOS 6

---
+++ emacsProfile/23.1.1/.emacs.d/site-lisp/site-lisp-entry.el
@@ -0,0 +1,19 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; kuntan2237's Emacs Profile
+;;; Site LIST Entry
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; ==========Compare Directory==========
+;; https://github.com/fourier/ztree.git
+;(add-to-list 'load-path' "~/.emacs.d/ztree")
+;(require 'ztree)
+
+;; ==========Load xcscope=========
+;; https://github.com/dkogan/xcscope.el.git
+(add-to-list 'load-path' "~/.emacs.d/xcscope.el")
+(require 'xcscope)
+    (define-key global-map [(control f1)]  'cscope-set-initial-directory)
+    (define-key global-map [(control f2)]  'cscope-unset-initial-directory)
+(cscope-setup)
+
+(provide 'site-lisp-entry)
