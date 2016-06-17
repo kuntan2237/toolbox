7986250330117e113f90015a1884a318e20abaaa
Kun Tan <kuntan2237@gmail.com>
2016-06-17 16:59:21 +0800

    Add set-title() for Gnome3 terminal

---
+++ gadget/set-terminal-title.sh
@@ -0,0 +1,17 @@
+#!/bin/env bash
+
+grep set-title ~/.bashrc > /dev/null
+if [ $? = 0 ]; then
+    echo "set-title() already exist."
+elif [ $? = 1 ]; then
+    echo 'set-title(){' >> ~/.bashrc
+    echo '    ORIG=$PS1' >> ~/.bashrc
+    echo '    TITLE="\e]2;$@\a"' >> ~/.bashrc
+    echo '    PS1=${ORIG}${TITLE}' >> ~/.bashrc
+    echo '}' >> ~/.bashrc
+    echo "set-title() patched into ~/.bashrc."
+elif [ $? = 2 ]; then
+    echo "~/.bashrc does not exist!"
+else
+    echo "Unknow error"
+fi
