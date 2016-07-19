c27c9027e11ba8f546bff1a52527801d4d64d700
Kun Tan <kuntan2237@gmail.com>
2016-07-19 17:17:23 +0800

    Add gadgit for sqlite test

---
+++ gadget/sqlite.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import sqlite3
+
+con = sqlite3.connect(sys.argv[1])
+con.isolation_level = None
+cur = con.cursor()
+
+buffer = ""
+
+print("Enter your SQL commands to execute in sqlite3.")
+print("Enter a blank line to exit.")
+
+while True:
+    line = input()
+    if line == "":
+        break
+    buffer += line
+    if sqlite3.complete_statement(buffer):
+        try:
+            buffer = buffer.strip()
+            cur.execute(buffer)
+
+            if buffer.lstrip().upper().startswith("SELECT"):
+                print(cur.fetchall())
+        except sqlite3.Error as e:
+            print("An error occurred:", e.args[0])
+        buffer = ""
+    else:
+        print("A SQL statement should terminaled by semicolons.")
+
+con.close()
